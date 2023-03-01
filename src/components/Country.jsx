a713f6a7c3544f49b80b1deb3da0c71c16195c34
Sreya Gadepally <sreyagadepally@gmail.com>
2023-03-01 13:21:01 +0530

    rendered array of objects in Country page
    Signed-off-by: Sreya Gadepally <sreyagadepally@gmail.com>

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -4,10 +4,21 @@ const Country = (props) => {
 
     console.log(currencies);
     
-    const renderList = (arr) => {
-        return arr.reduce((acc, current) => {
-            return acc.concat(current).concat(",");
-        }, "");
+    const renderList = (arr, flag) => {
+        switch (flag) {
+            case "nativeName":
+                return arr.reduce((acc, current) => {
+                    return acc.concat(current.common).concat(", ");
+                }, "");
+            case "currencies":
+                return arr.reduce((acc, current) => {
+                    return acc.concat(current.name).concat(", ");
+                }, "");
+            case "languages":
+                return arr.reduce((acc, current) => {
+                    return acc.concat(current).concat(", ");
+                }, "");
+        }
     };
 
     return (
@@ -15,16 +26,16 @@ const Country = (props) => {
             <img className="object-cover" src={flags.svg} alt={`${name.common} flag`}></img>
             <div className="sm:px-8 sm:pt-16 lg:px-4 lg:pt-0 lg:justify-self-center">
                 <h3 className="Nunito text-3xl mb-4 font-extrabold">{`${name.common}`}</h3>
-                <p className="Nunito text-xl mb-4">Native Name: {renderList(Object.values(name.nativeName))}</p>
+                <p className="Nunito text-xl mb-4">Native Name: {renderList(Object.values(name.nativeName), "nativeName")}</p>
                 <p className="Nunito text-xl mb-4">Region: {`${region}`}</p>
                 <p className="Nunito text-xl mb-4">Capital: {`${capital}`}</p>
-                <p className="Nunito text-xl mb-4">Currencies: {`${renderList(Object.values(currencies))}`}</p>
+                <p className="Nunito text-xl mb-4">Currencies: {`${renderList(Object.values(currencies), "currencies")}`}</p>
             </div>
             <div className="sm:px-8 lg:px-0 lg:pt-12">
                 <p className="Nunito text-xl mb-4">Population: {`${population}`}</p>
                 <p className="Nunito text-xl mb-4">Sub Region: {`${subregion}`}</p>
                 <p className="Nunito text-xl mb-4">Top Level Domain: {`${tld}`}</p>
-                <p className="Nunito text-xl mb-4">Languages: {renderList(Object.values(languages))}</p>
+                <p className="Nunito text-xl mb-4">Languages: {renderList(Object.values(languages), "languages")}</p>
             </div>
         </div>
     );
